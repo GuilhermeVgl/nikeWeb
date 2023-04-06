e16ef98d15ba11956bce4078ea9e9fd057949562
Guilherme Vieira Moreira de Freitas <83255622+GuilhermeVgl@users.noreply.github.com>
2023-04-06 00:04:01 -0400

    Add files via upload

---
+++ src/McTrainer/mct.js
@@ -0,0 +1,93 @@
+function expandir(small) {
+    var full = document.getElementById("imagebox")
+    full.src = small.src
+}
+
+function changeimg1(){
+
+    var img = document.getElementById("imagebox");
+    img.src = "img/shoe.webp";
+
+    var img = document.getElementById("shoe1");
+    img.src = "img/shoe.webp";
+
+    var img = document.getElementById("shoe2");
+    img.src = "img/shoe2.webp";
+
+    var img = document.getElementById("shoe3");
+    img.src = "img/shoe3.webp";
+
+    var img = document.getElementById("shoe4");
+    img.src = "img/shoe4.webp";
+
+    var img = document.getElementById("shoe5");
+    img.src = "img/shoe5.webp";
+
+    var img = document.getElementById("shoe6");
+    img.src = "img/shoe6.webp";
+
+    var img = document.getElementById("shoe7");
+    img.src = "img/shoe7.webp";
+
+    var img = document.getElementById("shoe8");
+    img.src = "img/shoe8.webp";
+
+}
+
+function changeimg2() {
+    var img = document.getElementById("imagebox");
+    img.src = "img2/shoe.webp";
+
+    var img = document.getElementById("shoe1");
+    img.src = "img2/shoe.webp";
+
+    var img = document.getElementById("shoe2");
+    img.src = "img2/shoe2.webp";
+
+    var img = document.getElementById("shoe3");
+    img.src = "img2/shoe3.webp";
+
+    var img = document.getElementById("shoe4");
+    img.src = "img2/shoe4.webp";
+
+    var img = document.getElementById("shoe5");
+    img.src = "img2/shoe5.webp";
+
+    var img = document.getElementById("shoe6");
+    img.src = "img2/shoe6.webp";
+
+    var img = document.getElementById("shoe7");
+    img.src = "img2/shoe7.webp";
+
+    var img = document.getElementById("shoe8");
+    img.src = "img2/shoe8.webp";
+}
+
+function changeimg3() {
+    var img = document.getElementById("imagebox");
+    img.src = "Preto/shoe.webp";
+
+    var img = document.getElementById("shoe1");
+    img.src = "Preto/shoe.webp";
+
+    var img = document.getElementById("shoe2");
+    img.src = "Preto/shoe2.webp";
+
+    var img = document.getElementById("shoe3");
+    img.src = "Preto/shoe3.webp";
+
+    var img = document.getElementById("shoe4");
+    img.src = "Preto/shoe4.webp";
+
+    var img = document.getElementById("shoe5");
+    img.src = "Preto/shoe5.webp";
+
+    var img = document.getElementById("shoe6");
+    img.src = "Preto/shoe6.webp";
+
+    var img = document.getElementById("shoe7");
+    img.src = "Preto/shoe7.webp";
+
+    var img = document.getElementById("shoe8");
+    img.src = "Preto/shoe8.webp";
+}
